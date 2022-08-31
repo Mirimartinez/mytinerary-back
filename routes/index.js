bebc1016231c4eff6ad880ef34d1a48baa513dff
zacarias-gabriel <gabriel14zk@hotmail.com>
2022-08-31 05:07:11 -0300

    se actualiza router con las rutas

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,28 @@
 var express = require('express');
 var router = express.Router();
+const cityRouter = require('./city')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json([])
+  /* res.json([]) */
+  res.render('index', { title: 'cities'})
 });
 
 
 
-router.get('/:id', function(req, res, next) {
+router.use('/city',cityRouter)
+
+
+
+/* router.get('/:id', function(req, res, next) {
   
   res.json({
     id : req.params.id
   });
-});
+}); */
+
+
+
 
 
 module.exports = router;
