dbb4678adb1467945fb3e6f628ff81e7cea26210
zacarias-gabriel <gabriel14zk@hotmail.com>
2022-08-31 16:11:21 -0300

    se crea y modifica City.js

--- models/City.js
+++ models/City.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const schema = new mongoose.Schema({
 
         city:{type: String, required: true},
-        coutry:{type: String, required: true},
+        country:{type: String, required: true},
         photo:{type: String, required: true},
         population:{type: Number, required: true},
         fundation:{type: Date, required: true}
