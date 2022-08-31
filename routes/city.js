f7a9976d154d82aae01ddfd88f1b034f5f63090b
zacarias-gabriel <gabriel14zk@hotmail.com>
2022-08-31 17:57:44 -0300

    se reemplazo city

--- routes/city.js
+++ routes/city.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const {create,read} = require('../controllers/CityController')
+const {create,read} = require('../controllers/Cities')
 
 
 
