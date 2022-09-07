79ec342ff33f21e2022d332ed1dd956199314139
Miriana Martinez <76982588+Mirimartinez@users.noreply.github.com>
2022-09-07 05:20:22 -0300

    Revert "B22a"

---
+++ routes/city.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+const {create} = require('../controllers/cityController')
+
+
+
+router.post('/',create)
+
+
+module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,8 @@
 var express = require('express');
 var router = express.Router();
-const cityRouter = require('./cities')
-
-
+const cityRouter = require('./city')
 
 /* GET home page. */
-
 router.get('/', function(req, res, next) {
   /* res.json([]) */
   res.render('index', { title: 'cities'})
@@ -13,7 +10,7 @@ router.get('/', function(req, res, next) {
 
 
 
-router.use('/cities',cityRouter)
+router.use('/city',cityRouter)
 
 
 
