952c086baacfdbc219bb2d925cc289327a161f8f
Teje8 <jteruel8@gmail.com>
2022-09-08 03:51:54 -0300

    fix model

--- models/Comment.js
+++ models/Comment.js
@@ -2,10 +2,9 @@ const mongoose = require('mongoose')
 
 
 const commentsSchema = new mongoose.Schema({
-
         comment:{type: String, required: true},
-        user:{type: String, required: false},
-        itinerary:{type: String, required: false}
+        user:{type: mongoose.Types.ObjectId, ref: 'users'},
+        itinerary:{type: mongoose.Types.ObjectId, ref: 'itineraries'}
 })
 
 const CommentModel = mongoose.model(
