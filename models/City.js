72561bd502d174af72941e4ad73de8c4e0e20ab6
Teje8 <jteruel8@gmail.com>
2022-09-08 03:51:34 -0300

    fix model

--- models/City.js
+++ models/City.js
@@ -4,17 +4,15 @@ const mongoose = require ('mongoose')
 const schema = new mongoose.Schema({
 
         city:{type: String, required: true},
-        coutry:{type: String, required: true},
+        country:{type: String, required: true},
         photo:{type: String, required: true},
         population:{type: Number, required: true},
-        fundation:{type: Date, required: true}
-
+        foundation:{type: Date, required: true}
 })
 
 const CityModel = mongoose.model(
     'city',
     schema
 )
-
 
 module.exports = CityModel
