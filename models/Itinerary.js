182247c1b000f00750493a55fbcbcef09a8fb477
Teje8 <jteruel8@gmail.com>
2022-09-08 03:52:35 -0300

    fix model

--- models/Itinerary.js
+++ models/Itinerary.js
@@ -2,15 +2,13 @@ const mongoose = require('mongoose')
 
 
 const itinerariesSchema = new mongoose.Schema({
-
         name:{type: String, required: true},
-        user:{type: String, required: false},
-        city:{type: String, required: false},
-        price:{type: Number, required: true},
+        user:{type: mongoose.Types.ObjectId, ref: 'users'},
+        city:{type: mongoose.Types.ObjectId, ref: 'cities'},
+        price:{type: Number, min:1, max:5, required: true},
         likes:{type: Array, required: true},
         tags:{type: Array, required: true},
         duration:{type: Number, required: true}
-
 })
 
 const ItineraryModel = mongoose.model(
