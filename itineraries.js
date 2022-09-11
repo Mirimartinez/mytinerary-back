ad7a0b202acd0d47f187578109bda1c4fc9c41f1
Miriana Martinez <mirianamartinezok@gmail.com>
2022-09-11 17:09:10 -0300

    agrega script de itinerarios

--- itineraries.js
+++ itineraries.js
@@ -7,101 +7,164 @@ const ItineraryModel = require("./models/City.js")
 ItineraryModel.create(
     {
     name:"Top of the rock observation deck",
-    user:"",
-    city:"New York",
-    price:"34",
-    likes:[],
-    tags:[skyline, view, NewYork, topOfTheRock, photography],
-    duration:"25"
+    user:"6319571d8f9f57ddc6a81f8c",
+    city:"63103ae1c8ea1e725dfbd22c",
+    price:34,
+    likes:[35],
+    tags:["skyline", "view", "NewYork", "topOfTheRock", "photography"],
+    duration:2
 },
 {
-    name:"Staten Island Ferry",
-    user:"",
-    city:"New York",
-    price:"30",
-    likes:[],
-    tags:[statenIsland, statueOfLiberty, siny, statenIslandFerry],
-    duration:"2"
+    name:"Staten Island",
+    user:"6319571d8f9f57ddc6a81f8c",
+    city:"63103ae1c8ea1e725dfbd22c",
+    price:30,
+    likes:[14],
+    tags:["statenIsland", "statueOfLiberty", "siny", "statenIslandFerry"],
+    duration:4
 },
 {
     name:"One World Observatory",
-    user:"",
-    city:"New York",
-    price:"38",
-    likes:[],
-    tags:[worldTradeCenter, wtc, manhattan, freedomTower, twintowers],
-    duration:"20"
+    user:"6319571d8f9f57ddc6a81f8c",
+    city:"63103ae1c8ea1e725dfbd22c",
+    price:38,
+    likes:[19],
+    tags:["oneworld", "wtc", "manhattan", "freedomTower", "twintowers"],
+    duration:3
 },
 {
-    name:"Visit to the Eiffel Tower",
-    user:"",
-    city:"Paris",
-    price:"26",
+    name:"Eiffel Tower",
+    user:"631d024516daebe0c7bf858c",
+    city:"63103ae1c8ea1e725dfbd231",
+    price:26,
     likes:[],
-    tags:[eiffelTower, paris, tourEiffel, parismonamour, visitparis],
-    duration:"45"
+    tags:["eiffelTower", "paris", "tourEiffel", "parismonamour", "visitparis"],
+    duration:1
 },
 {
-    name:"The Louvre tour",
-    user:"",
-    city:"Paris",
-    price:"15",
+    name:"The Museum tour",
+    user:"631d024516daebe0c7bf858c",
+    city:"63103ae1c8ea1e725dfbd231",
+    price:15,
     likes:[],
-    tags:[louvre, louvremuseum, museedulouvre, parisjetaime],
-    duration:"1"
+    tags:["louvre", "louvremuseum", "museedulouvre", "parisjetaime"],
+    duration:2
 },
 {
-    name:"Ride the London Eye",
-    user:"",
-    city:"London",
-    price:"27",
+    name:"Palace of Versailles",
+    user:"631d024516daebe0c7bf858c",
+    city:"63103ae1c8ea1e725dfbd231",
+    price:0,
     likes:[],
-    tags:[londoneye, londonlife, visitlondon, londonbridge,londoncity],
-    duration:"40"
+    tags:["versailles", "versaillespalace", "paris", "parisjetaime"],
+    duration:3
 },
 {
-    name:"Westminster Abbey",
-    user:"",
-    city:"London",
-    price:"24",
+    name:"London Eye",
+    user:"631d024516daebe0c7bf858c",
+    city:"63103ae1c8ea1e725dfbd22f",
+    price:27,
     likes:[],
-    tags:[westminsterabbey, westminster, england, westminsterpalace],
-    duration:"30"
+    tags:["londoneye", "londonlife", "visitlondon", "londonbridge", "londoncity"],
+    duration:2
 },
 {
-    name:"Full-Day Mount Fuji and Kawaguchi Lake Tour with Lunch",
-    user:"",
-    city:"Tokyo",
-    price:"65",
+    name:"Buckingham Palace",
+    user:"631d024516daebe0c7bf858c",
+    city:"63103ae1c8ea1e725dfbd22f",
+    price:24,
     likes:[],
-    tags:[mountfuji, tokyo, japan, kawaguchi, laketour, lovingtokyo],
-    duration:"11"
+    tags:["westminsterabbey", "westminster", "england", "westminsterpalace"],
+    duration:2
+},
+{
+    name:"Harry Potter",
+    user:"631d024516daebe0c7bf858c",
+    city:"63103ae1c8ea1e725dfbd22f",
+    price:30,
+    likes:[],
+    tags:["harrypotter", "hp", "england", "potterhead"],
+    duration:5
+},
+{
+    name:"Mount Fuji and Kawaguchiko",
+    user:"6319571d8f9f57ddc6a81f8c",
+    city:"63103ae1c8ea1e725dfbd22d",
+    price:65,
+    likes:[],
+    tags:["mountfuji", "tokyo", "japan", "kawaguchi", "laketour", "lovingtokyo"],
+    duration:11
+},
+{
+    name:"Shinbashi",
+    user:"6319571d8f9f57ddc6a81f8c",
+    city:"63103ae1c8ea1e725dfbd22d",
+    price:182,
+    likes:[],
+    tags:["shinbashi", "tokyo", "japan", "foodie", "tokyoatnight", "lovingtokyo"],
+    duration:3
 },
 {
     name:"Universal Studios Hollywood",
-    user:"",
-    city:"Los Angeles",
-    price:"118",
+    user:"631a376a076cbcfb4a7e5472",
+    city:"63103ae1c8ea1e725dfbd238",
+    price:118,
     likes:[],
-    tags:[universalstudios, hollywood, harrypotter, themepark, vacation],
-    duration:"9"
+    tags:["universalstudios", "hollywood", "harrypotter", "themepark", "vacation"],
+    duration:9
 },
 {
-    name:"Isla Mujeres Catamaran Tour",
-    user:"",
-    city:"Cancun",
-    price:"60",
+    name:"Bus tour",
+    user:"631a376a076cbcfb4a7e5472",
+    city:"63103ae1c8ea1e725dfbd238",
+    price:0,
     likes:[],
-    tags:[cancun, mexico, tulum, cancunmexico, beach, rivieramaya],
-    duration:"10"
+    tags:["hollywood", "hollywoodsign", "lasign", "losangeles"],
+    duration:1
 },
 {
-    name:"Royal Palace Guided Tour",
-    user:"",
-    city:"Turin",
-    price:"55",
+    name:"Isla Mujeres",
+    user:"631a376a076cbcfb4a7e5472",
+    city:"63103ae1c8ea1e725dfbd23b",
+    price:60,
     likes:[],
-    tags:[turin, italy, torinotoday, architecture],
-    duration:"2"
+    tags:["cancun", "mexico", "tulum", "cancunmexico", "mexicanisland", "catamarantour"],
+    duration:10
+},
+{
+    name:"Chichen Itza",
+    user:"631a376a076cbcfb4a7e5472",
+    city:"63103ae1c8ea1e725dfbd23b",
+    price:26,
+    likes:[],
+    tags:["cancun", "mexico", "chichenitza", "cancunmexico", "history"],
+    duration:6
+},
+{
+    name:"Turin's Royal Palace",
+    user:"631d024516daebe0c7bf858c",
+    city:"6310e83947b1f91d97174ae9",
+    price:55,
+    likes:[],
+    tags:["turin", "italy", "torinotoday", "architecture"],
+    duration:2
+},
+{
+    name:"Sydney Opera House",
+    user:"631d024516daebe0c7bf858c",
+    city:"63103ae1c8ea1e725dfbd232",
+    price:43,
+    likes:[],
+    tags:["sydney", "operahouse", "australia", "sydneyicon"],
+    duration:2
+},
+{
+    name:"Royal Palace and Del Prado museum",
+    user:"631a376a076cbcfb4a7e5472",
+    city:"63103ae1c8ea1e725dfbd22e",
+    price:39,
+    likes:[],
+    tags:["madrid", "royalpalace", "spaintour", "lovingmadrid"],
+    duration:3
 }
 )
