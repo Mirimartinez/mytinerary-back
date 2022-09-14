7903510be2485f3adbab9c82967c63735e633628
Teje8 <jteruel8@gmail.com>
2022-09-14 04:47:32 -0300

    add route new controller

--- routes/itineraries.js
+++ routes/itineraries.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const {create, update, destroy, all} = require('../controllers/itineraryController')
+const {create, update, destroy, all, getItinerary} = require('../controllers/itineraryController')
 
 router.post('/', create)
 router.get('/', all)
 router.patch('/:id', update)
 router.delete('/:id', destroy)
+router.get('/:id', getItinerary)
 
 
 module.exports = router;
