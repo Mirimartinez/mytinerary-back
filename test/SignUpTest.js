54334d636df9e64f0f5b13a73e1732ee5faa7166
Miriana Martinez <mirianamartinezok@gmail.com>
2022-09-15 18:56:50 -0300

    signUp test correcto

--- test/SignUpTest.js
+++ test/SignUpTest.js
@@ -40,47 +40,11 @@ const { assert } = require('chai')
 
     it('Must respond with 400 status code', function(done){
         request(app)
-            .post('/cities')
+            .post('/signin')
             .send({})
             .expect(400, done)
             
     })
 
 
-     it('Must respond with 404 status code', function (done) {
-         request(app)
-             .post('/auth/signin')
-             .send({
-                 mail:'mirianamartinezok@gmail.com' ,
-                 password: 'bye123',
-                 from: 'form'
-             })
-             .expect(404, done)
-     }),
-
-     it('Must respond with 201 status code', function (done) {
-         request(app)
-             .post('/auth/signin')
-             .send({
-                 mail:'marcosamu9@gmail.com' ,
-                 password: '12343242332432np',
-                 from: 'form'
-             })
-             .expect(200, done)
-     })
-
-     it('Must respond with 201 status code', function (done) {
-         request(app)
-             .post('/auth/signin')
-             .send({
-                 mail:'amuchasteguiesequiel@gmail.com' ,
-                 password: '123456789ese',
-                 from: 'google'
-             })
-             .then(response => {
-                 console.log(response.body.id)
-                 assert.isString(response.body.id)
-                 done()
-             }).catch(done)
-         })
      })
