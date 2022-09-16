1a181cb60c1729dbeb93dda3f10c8f4b2aa660a4
Teje8 <jteruel8@gmail.com>
2022-09-16 06:35:41 -0300

    delete limit password

--- models/User.js
+++ models/User.js
@@ -1,78 +1,21 @@
-const mongoose = require('mongoose')
-
+const mongoose = require("mongoose");
 
 const usersSchema = new mongoose.Schema({
+    name: {type: String, required: true, min: 5, max: 75,},
+    lastName: {type: String, required: true, min: 5, max: 75,},
+    mail: {type: String, required: true,},
+    password: [{type: String, required: true,},],
+    photo: {type: String, required: true, validate: function (value) {
+        if (!value.startsWith("http")) {
+            throw new Error('The URL must be start with "http"');}},},
+    country: {type: String, required: true, min: 5, max: 25,},
+    from: [{type: String, required: true,},],
+    logged: {type: String, required: false,},
+    verified: {type: String,required: false,},
+    code: {type: String,required: false,},
+    role: {type: String,required: true,},
+    });
 
-        name:{
-            type: String, 
-            required: true,
-            min: 5,
-            max:75
-        },
+const UserModel = mongoose.model("users", usersSchema);
 
-        lastName:{
-            type: String, 
-            required: true,
-            min: 5,
-            max:75
-        },
-
-        mail:{
-            type: String, 
-            required: true
-        },
-
-        password:[{
-            type: String, 
-            required: true,
-        }]
-        ,
-        photo:{
-            type: String, 
-            required: true,
-            validate: function (value) {
-                if (! value.startsWith('http')) {
-                    throw new Error('The URL must be start with "http"')
-                }
-            }
-        },
-
-        country:{
-            type: String, 
-            required: true,
-            min: 5,
-            max:25
-        },
-
-        from: [{
-            type: String, 
-            required: true
-        }],
-
-        logged: {
-            type: String, 
-            required: false
-        },
-
-        verified: {
-            type: String, 
-            required: false
-        },
-
-        code: {
-            type: String, 
-            required: false
-        },
-
-        role: {
-            type: String,
-            required: true
-        }
-    })
-
-const UserModel = mongoose.model(
-    'users',
-    usersSchema
-)
-
-module.exports = UserModel
+module.exports = UserModel;
