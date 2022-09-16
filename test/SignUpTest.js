bc012d1bb2223b4db4364f6c0adbeac2f8e6464b
Miriana Martinez <mirianamartinezok@gmail.com>
2022-09-16 01:05:31 -0300

    test running correctly

--- test/SignUpTest.js
+++ test/SignUpTest.js
@@ -1,50 +1,52 @@
-const request = require('supertest')
-const app = require('../app')
-const { assert } = require('chai')
+   const request = require('supertest')
+   const app = require('../app')
+   const { assert } = require('chai')
 
- describe('POST /signup', function () {
+    describe('POST /signup', function () {
 
-    it('Must respond with the id', function(done){
-        request(app)
-            .post('/signin')
-            .send({
-                name:"Miri",
-            lastName:"Martinez",
-            mail:"mirianamartinezokgmail.com",
-            password: "bye123",
-            photo: "https://static.vecteezy.com/system/resources/previews/008/005/254/non_2x/girl-head-in-profile-icon-avatar-login-isolated-icon-illustration-vector.jpg",
-            country: "Argentina"
+       it('Must respond with the id', function(){
+           request(app)
+               .post('/auth/signup')
+               .send({
+                   name:"Steven",
+                   lastName:"Tyler",
+                   mail:"stevensmokingtyler@gmail.com",
+                   password:"AerosmithRocks!",
+                   photo:"https:  upload.wikimedia.org/wikipedia/commons/thumb/a/a8/Steven_Tyler_by_Gage_Skidmore_3.jpg/330px-Steven_Tyler_by_Gage_Skidmore_3.jpg",
+                   country:"United States",
+                   from: "form"
+               })  
+               .then(response => {
+                   city = response.body.id
+                   assert.isString(response.body.id)
+                   done()
+                   })
+       })
+
+       it('Must respond with 200 status code', function(done){
+           request(app)
+               .post('/auth/signup')
+               .send({
+                   name:"Steven",
+                   lastName:"Tyler",
+                   mail:"stevensmokingtyler@gmail.com",
+                   password:"AerosmithRocks!",
+                   photo:"https:  upload.wikimedia.org/wikipedia/commons/thumb/a/a8/Steven_Tyler_by_Gage_Skidmore_3.jpg/330px-Steven_Tyler_by_Gage_Skidmore_3.jpg",
+                   country:"United States",
+                   from: "form"
+               })
+               .expect(200, done)
             
-            })  
-            .expect(201)
-            .end(function (err, res){
-                if(err) return done(err);
-                return done();
-            })
-    })
+       })
 
-    it('Must respond with 201 status code', function(done){
-        request(app)
-            .post('/signin')
-            .send({
-                name:"Miri",
-                lastName:"Martinez",
-                mail:"mirianamartinezokgmail.com",
-                password: "bye123",
-                photo: "https://static.vecteezy.com/system/resources/previews/008/005/254/non_2x/girl-head-in-profile-icon-avatar-login-isolated-icon-illustration-vector.jpg",
-                country: "Argentina"
-            })
-            .expect(201, done)
+       it('Must respond with 404 status code', function(done){
+           request(app)
+               .post('/signup')
+               .send({})
+               .expect(404, done)
             
-    })
-
-    it('Must respond with 400 status code', function(done){
-        request(app)
-            .post('/signin')
-            .send({})
-            .expect(400, done)
             
-    })
+       })
 
 
-     })
+      })
