3e00242444f3ffe7ea67ec09610aea5557663e5f
Miriana Martinez <mirianamartinezok@gmail.com>
2022-09-16 01:05:51 -0300

    test running correctly

--- test/citiestest.js
+++ test/citiestest.js
@@ -1,44 +1,48 @@
-const request = require('supertest')
-const app = require('../app')
-const { assert } = require('chai')
+  const request = require('supertest')
+  const app = require('../app')
+  const {assert} = require('chai')
 
-describe('POST /cities', function(){
-    it('Must respond with the id', function(done){
-        request(app)
-            .post('/cities')
-            .send({
-                city:"city",
-            country:"country",
-            photo: "https://i.ytimg.com/vi/O6Cg4ing5bo/maxresdefault.jpg",
-            population:12121212,
-            fundation:1212
-            })  
-            .expect(201)
-            .end(function (err, res){
-                if(err) return done(err);
-                return done();
-            })
-    })
+  describe('POST /cities', function () {
+      it('Must respond with the id', function () {
+          request(app)
+              .post('/cities')
+              .send({
+                  city: "New York",
+      country:"United States",
+      photo:"https: estaticos.muyinteresante.es/uploads/images/test/60b4a8d15cafe819e843397a/empire-state-redes.jpg",
+      population:18867000,
+      foundation:1624
+              })
+              .then(response => {
+                  city = response.body.id
+                  assert.isString(response.body.id)
+                  done()
+              })
+      })
 
-    it('Must respond with 201 status code', function(done){
-        request(app)
-            .post('/cities')
-            .send({
-            city:"city",
-            country:"country",
-            photo: "https://i.ytimg.com/vi/O6Cg4ing5bo/maxresdefault.jpg",
-            population:12121212,
-            fundation:1212
-            })
-            .expect(201, done)
-            
-    })
+      it('Must respond with 201 status code', function (done) {
+          request(app)
+              .post('/cities')
+              .send({
+                  city: "New York",
+                  country:"United States",
+                  photo:"https: estaticos.muyinteresante.es/uploads/images/test/60b4a8d15cafe819e843397a/empire-state-redes.jpg",
+                  population:18867000,
+                  foundation:1624
+              })
+              .expect(201, done)
+      })
 
-    it('Must respond with 400 status code', function(done){
-        request(app)
-            .post('/cities')
-            .send({})
-            .expect(400, done)
-            
-    })
-})
+      it('Must respond with 400 status code', function (done) {
+          request(app)
+              .post('/cities')
+              .send({
+                  city: "Miami"
+              })
+              .expect(400)
+              .end(function (err, res) {
+                  if (err) return done(err);
+                  return done();
+              })
+      })
+  })
