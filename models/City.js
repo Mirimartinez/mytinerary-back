0206d746b525ddbe9ac4839a05910d26eea94248
Miriana Martinez <mirianamartinezok@gmail.com>
2022-09-23 05:21:58 -0300

    add latitude & longitude to city model

--- models/City.js
+++ models/City.js
@@ -3,34 +3,14 @@ const mongoose = require ('mongoose')
 
 const schema = new mongoose.Schema({
 
-        city:{
-            type: String, 
-            required: true,
-            minlength: 5,
-            maxlength: 100
-        },
-        country:{
-            type: String, 
-            required: true,
-            minlength: 6,
-            maxlength: 50
-        },
-        photo:{
-            type: String, 
-            required: true,
-            validate: function (value) {
-                if (! value.startsWith('http')) {throw new Error('URL must be start with http')
-                }
-            }
-        },
-        population:{
-            type: Number, 
-            required: true
-        },
-        foundation:{
-            type: Date, 
-            required: true
-        }
+        city:{type: String, required: true, minlength: 5, maxlength: 100},
+        country:{type: String, required: true, minlength: 6, maxlength: 100},
+        photo:{type: String, required: true, validate: function (value) {
+                if (! value.startsWith('http')) {throw new Error('URL must be start with http')}}},
+        latitude:{type: Number, required: true},
+        longitude: {type: Number, required: true},
+        population:{type: Number, required: true},
+        foundation:{type: Date, required: true}
 })
 
 const CityModel = mongoose.model(
