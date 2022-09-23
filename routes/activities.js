c0f3e6639585bc02aad9a219898879391de035e0
Miriana Martinez <mirianamartinezok@gmail.com>
2022-09-23 05:20:50 -0300

    add route edit & delete

--- routes/activities.js
+++ routes/activities.js
@@ -1,10 +1,13 @@
 let express = require('express')
 let router = express.Router()
-const {create, all, getActivity} = require('../controllers/activityController')
+const {create, all, getActivity, editActivity, deleteActivity} = require('../controllers/activityController')
 
 // GET users listing
 router.post('/', create)
 router.get('/', all)
-router.get('/', getActivity)
+router.get('/:id', getActivity)
+router.patch('/:id', editActivity);
+router.delete('/:id', deleteActivity);
+
 
 module.exports = router
--- routes/comment.js
+++ routes/comment.js
@@ -1,11 +1,12 @@
 let express = require('express')
 let router = express.Router()
-const {createComment, getComment, getAllComments, editComment, deleteComment} = require('../controllers/commentController')
+const {create, getComment, getAllComments, editComment, deleteComment} = require('../controllers/commentController')
 
 // GET users listing
-router.post('/', createComment);
+router.post('/', create);
 router.get('/', getAllComments);
 router.get('/:id', getComment);
 router.patch('/:id', editComment);
 router.delete('/:id', deleteComment);
+
 module.exports = router
