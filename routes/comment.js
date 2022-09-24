c3fb42f9ba1cbaae2f3388a3601f196c7aa9da8b
Teje8 <jteruel8@gmail.com>
2022-09-23 23:19:51 -0300

    fix route

--- routes/comment.js
+++ routes/comment.js
@@ -1,9 +1,12 @@
 let express = require('express')
 let router = express.Router()
-const {create, all} = require('../controllers/commentController')
+const {create, all, updateComment, deleteComment} = require('../controllers/commentController')
+const passport = require('../config/passport')
 
 // GET users listing
-router.post('/', create)
+router.post('/', passport.authenticate('jwt',{ session: false}), create)
 router.get('/', all)
+router.patch('/:id',passport.authenticate('jwt', {session:false}), updateComment)
+router.delete('/:id',passport.authenticate('jwt', {session:false}), deleteComment)
 
 module.exports = router
