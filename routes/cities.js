ce27c238495a055db85b7c1131eecb38363c3477
Teje8 <jteruel8@gmail.com>
2022-09-23 23:19:22 -0300

    fix route

--- routes/cities.js
+++ routes/cities.js
@@ -1,10 +1,11 @@
 var express = require ('express');
 var router = express.Router();
 const {createCity,readCity,readCities,updateCity,removeCity} = require('../controllers/Cities')
+const passport = require('../config/passport')
 
 
 
-router.post('/',createCity)
+router.post('/',passport.authenticate('jwt', {session:false}), createCity)
 
 router.get('/',readCities)
 
@@ -13,9 +14,9 @@ router.get('/',readCities)
 
 router.get('/:id',readCity)
 
-router.patch('/:id',updateCity)
+router.patch('/:id',passport.authenticate('jwt', {session:false}), updateCity)
 
-router.delete('/:id',removeCity)
+router.delete('/:id',passport.authenticate('jwt', {session:false}), removeCity)
 
 
 module.exports = router
