aaea675945b79e991fcffaa18ca214e43db57a88
Miriana Martinez <mirianamartinezok@gmail.com>
2022-09-24 21:18:37 -0300

    modify city in itinerary model

--- models/Itinerary.js
+++ models/Itinerary.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const itinerariesSchema = new mongoose.Schema({
         name:{type: String, required: true, minlength: 6,maxlength: 100},
         user:{type: mongoose.Types.ObjectId, ref: 'users'},
-        city:{type: mongoose.Types.ObjectId, ref: 'cities', minlength: 5,maxlength: 100},
+        city:{type: mongoose.Types.ObjectId, ref: 'city', minlength: 1,maxlength: 100},
         price:{type: Number, min:1, max:5, required: true},
         likes:[{type: Array, required: true}],
         tags:[{type: String, required: true}],
